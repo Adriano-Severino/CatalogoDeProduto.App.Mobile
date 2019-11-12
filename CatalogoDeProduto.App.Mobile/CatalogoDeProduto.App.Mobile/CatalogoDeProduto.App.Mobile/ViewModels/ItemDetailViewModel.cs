82e7f282c8f696da218aae8a50b11dc5c2b3aa46
Adriano Severino <adriano.asx@outlook.com>
2019-11-12 17:57:53 -0300

    Criaçào do Projeto

---
+++ CatalogoDeProduto.App.Mobile/CatalogoDeProduto.App.Mobile/CatalogoDeProduto.App.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CatalogoDeProduto.App.Mobile.Models;
+
+namespace CatalogoDeProduto.App.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
